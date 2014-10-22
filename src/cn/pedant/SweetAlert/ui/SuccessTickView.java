568c1cc6629b8b379d080021d426e585524f7916
pedant <yanyuetian@gmail.com>
2014-10-22 10:01:26 +0800

    optimize SuccessTickView parameter computing

--- src/cn/pedant/SweetAlert/ui/SuccessTickView.java
+++ src/cn/pedant/SweetAlert/ui/SuccessTickView.java
@@ -17,10 +17,13 @@ public class SuccessTickView extends View {
     private final float CONST_RECT_WEIGHT = dip2px(3);
     private final float CONST_LEFT_RECT_W = dip2px(15);
     private final float CONST_RIGHT_RECT_W = dip2px(25);
+    private final float MIN_LEFT_RECT_W = dip2px(3.3f);
+    private final float MAX_RIGHT_RECT_W = CONST_RIGHT_RECT_W + dip2px(6.7f);
 
-    private float mLeftAddWidth;
-    private float mRightAddWidth;
-    private boolean mLeftLineGrowMode;
+    private float mMaxLeftRectWidth;
+    private float mLeftRectWidth;
+    private float mRightRectWidth;
+    private boolean mLeftRectGrowMode;
 
     public SuccessTickView(Context context) {
         super(context);
@@ -35,9 +38,9 @@ public class SuccessTickView extends View {
     private void init () {
         mPaint = new Paint();
         mPaint.setColor(Color.parseColor("#A5DC86"));
-        mLeftAddWidth = CONST_LEFT_RECT_W;
-        mRightAddWidth = CONST_RIGHT_RECT_W;
-        mLeftLineGrowMode = false;
+        mLeftRectWidth = CONST_LEFT_RECT_W;
+        mRightRectWidth = CONST_RIGHT_RECT_W;
+        mLeftRectGrowMode = false;
     }
 
     @Override
@@ -50,15 +53,17 @@ public class SuccessTickView extends View {
 
         totalW /= 1.2;
         totalH /= 1.4;
+        mMaxLeftRectWidth = (totalW + CONST_LEFT_RECT_W) / 2 + CONST_RECT_WEIGHT - 1;
+
         RectF leftRect = new RectF();
-        if (mLeftLineGrowMode) {
+        if (mLeftRectGrowMode) {
             leftRect.left = 0;
-            leftRect.right = leftRect.left + mLeftAddWidth;
+            leftRect.right = leftRect.left + mLeftRectWidth;
             leftRect.top = (totalH + CONST_RIGHT_RECT_W) / 2;
             leftRect.bottom = leftRect.top + CONST_RECT_WEIGHT;
         } else {
             leftRect.right = (totalW + CONST_LEFT_RECT_W) / 2 + CONST_RECT_WEIGHT - 1;
-            leftRect.left = leftRect.right - mLeftAddWidth;
+            leftRect.left = leftRect.right - mLeftRectWidth;
             leftRect.top = (totalH + CONST_RIGHT_RECT_W) / 2;
             leftRect.bottom = leftRect.top + CONST_RECT_WEIGHT;
         }
@@ -69,7 +74,7 @@ public class SuccessTickView extends View {
         rightRect.bottom = (totalH + CONST_RIGHT_RECT_W) / 2 + CONST_RECT_WEIGHT - 1;
         rightRect.left = (totalW + CONST_LEFT_RECT_W) / 2;
         rightRect.right = rightRect.left + CONST_RECT_WEIGHT;
-        rightRect.top = rightRect.bottom - mRightAddWidth;
+        rightRect.top = rightRect.bottom - mRightRectWidth;
         canvas.drawRoundRect(rightRect, CONST_RADIUS, CONST_RADIUS, mPaint);
     }
 
@@ -82,36 +87,36 @@ public class SuccessTickView extends View {
 
     public void startTickAnim () {
         // hide tick
-        mLeftAddWidth = 0;
-        mRightAddWidth = 0;
+        mLeftRectWidth = 0;
+        mRightRectWidth = 0;
         invalidate();
         Animation tickAnim = new Animation() {
             @Override
             protected void applyTransformation(float interpolatedTime, Transformation t) {
                 super.applyTransformation(interpolatedTime, t);
-
-                if (0.54 < interpolatedTime && 0.7 >= interpolatedTime) {
-                    mLeftLineGrowMode = true;
-                    mLeftAddWidth = ((getWidth() / 1.2f + CONST_LEFT_RECT_W) / 2 + CONST_RECT_WEIGHT - 1) * ((interpolatedTime - 0.54f) / 0.16f);
+                if (0.54 < interpolatedTime && 0.7 >= interpolatedTime) {  // grow left and right rect to right
+                    mLeftRectGrowMode = true;
+                    mLeftRectWidth = mMaxLeftRectWidth * ((interpolatedTime - 0.54f) / 0.16f);
                     if (0.65 < interpolatedTime) {
-                        mRightAddWidth = (CONST_RIGHT_RECT_W + 10) * ((interpolatedTime - 0.65f) / 0.19f);
+                        mRightRectWidth = MAX_RIGHT_RECT_W * ((interpolatedTime - 0.65f) / 0.19f);
                     }
                     invalidate();
-                } else if (0.7 < interpolatedTime && 0.84 >= interpolatedTime) {
-                    mLeftLineGrowMode = false;
-                    mLeftAddWidth = ((getWidth() / 1.2f + CONST_LEFT_RECT_W) / 2 + CONST_RECT_WEIGHT - 1) * (1 - ((interpolatedTime - 0.7f) / 0.14f));
-                    mLeftAddWidth = mLeftAddWidth < 5 ? 5 : mLeftAddWidth;
-                    mRightAddWidth = (CONST_RIGHT_RECT_W + 10) * ((interpolatedTime - 0.65f) / 0.19f);
+                } else if (0.7 < interpolatedTime && 0.84 >= interpolatedTime) { // shorten left rect from right, still grow right rect
+                    mLeftRectGrowMode = false;
+                    mLeftRectWidth = mMaxLeftRectWidth * (1 - ((interpolatedTime - 0.7f) / 0.14f));
+                    mLeftRectWidth = mLeftRectWidth < MIN_LEFT_RECT_W ? MIN_LEFT_RECT_W : mLeftRectWidth;
+                    mRightRectWidth = MAX_RIGHT_RECT_W * ((interpolatedTime - 0.65f) / 0.19f);
                     invalidate();
-                } else if (0.84 < interpolatedTime && 1 >= interpolatedTime) {
-                    mLeftLineGrowMode = false;
-                    mLeftAddWidth = 5 + (CONST_LEFT_RECT_W - 5) * ((interpolatedTime - 0.84f) / 0.16f);
-                    mRightAddWidth = CONST_RIGHT_RECT_W + 10 * (1 - ((interpolatedTime - 0.84f) / 0.16f));
+                } else if (0.84 < interpolatedTime && 1 >= interpolatedTime) { // restore left rect width, shorten right rect to const
+                    mLeftRectGrowMode = false;
+                    mLeftRectWidth = MIN_LEFT_RECT_W + (CONST_LEFT_RECT_W - MIN_LEFT_RECT_W) * ((interpolatedTime - 0.84f) / 0.16f);
+                    mRightRectWidth = CONST_RIGHT_RECT_W + (MAX_RIGHT_RECT_W - CONST_RIGHT_RECT_W) * (1 - ((interpolatedTime - 0.84f) / 0.16f));
                     invalidate();
                 }
             }
         };
         tickAnim.setDuration(750);
+        tickAnim.setStartOffset(100);
         startAnimation(tickAnim);
     }
 }
